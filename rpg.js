11c9b5019ff6926ef0f850acd5a20b2732c95a85
jkwaser <jkwaser@gmail.com>
2022-03-06 22:19:20 +1000

    Added capitalisation function so any permutation of capitals/non capitals for Rock/Paper/Scissors work.

--- rpg.js
+++ rpg.js
@@ -4,12 +4,24 @@ function computerPlay() {
 }
 
 function compare(playerSelection, computerSelection) {
+
+    let playerSel = playerSelection.toUpperCase();
+    let compSel = computerSelection.toUpperCase();
+
     if (playerSelection == computerSelection) {
         return "Tie! ${playerSelection} and ${computerSelection} are the same!"
     }
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS" ||
-        playerSelection == "SCISSORS" && computerSelection == "PAPER" ||
-        playerSelection == "PAPER" && computerSelection == "ROCK") {
+    else if (playerSel == "ROCK" && compSel == "SCISSORS" ||
+    playerSel == "SCISSORS" && compSel == "PAPER" ||
+    playerSel == "PAPER" && compSel == "ROCK") {
             return "Victory for humanity!"
         }
+        
+    else if (playerSel == "PAPER" && compSel == "SCISSORS" ||
+    playerSel == "ROCK" && compSel == "PAPER" ||
+    playerSel == "SCISSORS" && compSel == "ROCK") {
+            return "The machine hordes march ever-closer to victory... you failed."
+        }
 }
+
+ // - Need to get this in all caps. 
