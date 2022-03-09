283038c0d81ab62a8d6c0663e64c9a83cf650245
jkwaser <jkwaser@gmail.com>
2022-03-09 22:54:12 +1000

    Debugged play function, now works with a manually set playerInput over all five games. Next to set up independent input each time.

--- rpg.js
+++ rpg.js
@@ -9,23 +9,45 @@ function playRound(playerSelection, computerSelection) {
     let compSel = computerSelection.toUpperCase();
 
     if (playerSel == compSel) {
-        return `Tie! ${playerSel} and ${compSel} are the same!`
+        return `Tie!`
     }
     else if (playerSel == "ROCK" && compSel == "SCISSORS" ||
     playerSel == "SCISSORS" && compSel == "PAPER" ||
     playerSel == "PAPER" && compSel == "ROCK") {
-            return "Victory for humanity!"
+            return "Victory!"
         }
         
     else if (playerSel == "PAPER" && compSel == "SCISSORS" ||
     playerSel == "ROCK" && compSel == "PAPER" ||
     playerSel == "SCISSORS" && compSel == "ROCK") {
-            return "The machine hordes march ever-closer to victory... you failed."
+            return "Loss."
         }
 
     else {
         return "This doesn't seem right... try again."
     }
+}
+
+function game() {
+    let compWin = 0;
+    let playWin = 0;
+    for (let i = 0; i < 5; i++) {
+        let checkWin = playRound(playerSelection, computerPlay());
+        if (checkWin == 'Victory!') {
+            i += 1;
+            playWin += 1
+        } 
+        else if (checkWin == 'Loss.') {
+            i += 1;
+            compWin += 1
+        }
+     }
+     if (playWin > compWin) {
+         return "Victory for humanity!"
+     }
+     else if (compWin > playWin) {
+         return "The robot hordes have overcome you..."
+     }
 }
 
  // - Need to get this in all caps. 
